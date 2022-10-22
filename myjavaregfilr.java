ab769366bacf5d3e4ce980e952e4bbb46f3ea540
anwar848 <sdanwar829@gmail.com>
2022-10-22 16:46:49 +0530

    javafilr

--- myjavaregfilr.java
+++ myjavaregfilr.java
@@ -1,2 +1,10 @@
 this is reg page
+<<<<<<< Updated upstream
 this is manager work
+=======
+this is frist day code 
+this is secpnd day code 
+this is third day code 
+this is fourth day code 
+this is fifth day code
+this is sixth day code 
